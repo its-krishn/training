e1335e9d06031f91b5c5a927297f6bcd3e00ca0b
its-krishn <101237499+its-krishn@users.noreply.github.com>
2022-03-09 13:01:24 +0530

    Update SpringBootUstApplication.java

--- src/main/java/com/stackroute/ustglobal/project/SpringBootUstApplication.java
+++ src/main/java/com/stackroute/ustglobal/project/SpringBootUstApplication.java
@@ -3,6 +3,7 @@ package com.stackroute.ustglobal.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//just try
 @SpringBootApplication
 public class SpringBootUstApplication {
 
