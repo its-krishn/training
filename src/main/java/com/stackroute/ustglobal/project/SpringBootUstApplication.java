f60967d8d94ed77e91dd9401cdef3dcb8f6f8e6f
krishnendu <krishnendukm123@gmail.com>
2022-03-09 12:54:47 +0530

    first commit

---
+++ src/main/java/com/stackroute/ustglobal/project/SpringBootUstApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.ustglobal.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackroute/ustglobal/project/SpringBootUstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.ustglobal.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
